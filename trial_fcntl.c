d236da5a2eaa97f30c0581f3a4a6a35f0b7fb2bf
maemo <maemo@maemo-TomXue>
2011-11-01 04:08:40 +0000

    basic fcntl function trial

---
+++ trial_fcntl.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MSG_TRY "try again\n"
+
+int main(void)
+{
+	char buf[10];
+	int n;
+	int flags;
+	flags = fcntl(STDIN_FILENO, F_GETFL);
+	flags |= O_NONBLOCK;	//set the standard input with non-block flag
+	if (fcntl(STDIN_FILENO, F_SETFL, flags) == -1) {
+		perror("fcntl");
+		exit(1);
+	}
+tryagain:
+	n = read(STDIN_FILENO, buf, 10);  //read input from the standard input
+	if (n < 0) {
+		if (errno == EAGAIN) {    //EAGAIN: try again
+			sleep(3);        //wait for 3 seconds
+			write(STDOUT_FILENO, MSG_TRY, strlen(MSG_TRY));
+			goto tryagain;	  //loop
+		}
+		perror("read stdin");
+		exit(1);
+	}
+	write(STDOUT_FILENO, buf, n);   //standard input -> stabdard output
+	return 0;
+}
